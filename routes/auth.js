3b24a7ff8a333e43175cd52bb85a0048dac7504f
Nastya <zelenenkaya@gmail.com>
2020-07-04 12:51:31 +0300

    add check auth in login / registration routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs');
 
 const router = new Router()
 
@@ -7,6 +8,8 @@ router.get('/login', async (req,res) => {
 	res.render('auth/login', {
 		title: 'Authorisation',
 		isLogin: true,
+		loginError: req.flash('loginError'),
+		regError: req.flash('regError')
 	})
 })
 
@@ -18,17 +21,57 @@ router.get('/logout', async (req,res) => {
 
 
 router.post('/login', async (req, res) => {
-	const user = await User.findById('5ef895a584b53a04168a2d2c')
-	req.session.user = user
-	req.session.isAuth = true
-	req.session.save(err => {
-		if (err) throw err
+	try {
+		const {email, password} = req.body
+		const candidate = await User.findOne({email})
 
-		res.redirect('/')
-	})
+		if (candidate) {
+			const areSame = await bcrypt.compare(password, candidate.password)
+
+			if (areSame) {
+				req.session.user = candidate
+				req.session.isAuth = true
+				req.session.save(err => {
+					if (err) throw err
+
+					res.redirect('/')
+				})
+			} else {
+				req.flash('loginError', "Wrong password")
+				res.redirect('/auth/login#login')
+			}
+		} else {
+			req.flash('loginError', "User with this email doesn't exist")
+			res.redirect('/auth/login#login')
+		}
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 router.post('/register', async (req, res) => {
+	try {
+		const {email, password, repeat, name} = req.body
 
+		const candidate = await User.findOne({email})
+		if (candidate) {
+			req.flash('regError', 'User with this email already exist')
+			res.redirect('/auth/login#register')
+		} else {
+			const hashPassword = await bcrypt.hash(password, 10)
+			const user = new User({
+				email,
+				name,
+				password: hashPassword,
+				cart: {
+					items: []
+				}})
+			await user.save()
+			res.redirect('/auth/login#login')
+		}
+	} catch (err) {
+		console.log(err)
+	}
 })
+
 module.exports = router
