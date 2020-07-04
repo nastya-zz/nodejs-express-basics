80fb6be2e477f742b9c1b3b07dfece14fbd2d8ca
Nastya <zelenenkaya@gmail.com>
2020-07-04 12:49:35 +0300

    add auth middleware

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const Course = require('../models/course');
+const auth = require('../middleware/auth')
 const router = Router();
 
 router.get('/', async (req, res) => {
@@ -16,7 +17,7 @@ router.get('/', async (req, res) => {
     });
 });
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', auth,  async (req, res) => {
     if (!req.query.allow) {
         return res.redirect('/');
     }
@@ -33,7 +34,7 @@ router.get('/:id/edit', async (req, res) => {
 
 });
 
-router.post('/edit', async (req, res) => {
+router.post('/edit', auth, async (req, res) => {
     const { id } = req.body
     delete req.body.id
 
@@ -58,7 +59,7 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
-router.post('/remove', async (req, res) => {
+router.post('/remove', auth,  async (req, res) => {
     try {
         await Course.deleteOne({_id: req.body.id})
         res.redirect('/courses')
