8b7cea4fb0ae401a99cfecf457c0b4d86d593604
Nastya <zelenenkaya@gmail.com>
2020-07-05 00:05:48 +0300

    add global constants

---
+++ keys/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MONGODB_URI: `mongodb+srv://Nastya:8uLbs7tJ8HvpHG2U@cluster0-ajlou.mongodb.net/shop?retryWrites=true&w=majority`,
+	SESSION_SECRET: 'some secret value',
+	SEND_GRID_API_KEY: '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',
+	EMAIL_FROM: 'zelenenkaya9@yandex.ru',
+	BASE_URL: 'http://localhost:3000'
+}
